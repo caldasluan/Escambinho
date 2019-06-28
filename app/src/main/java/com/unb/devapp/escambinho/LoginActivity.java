5b1e9ceb986f37c0114cba39e2795f54d5c7aa67
Marcos Farias <20694062+fariasmarcos@users.noreply.github.com>
2019-06-28 05:07:44 -0300

    Add fisrt rev of Firebase's Auth Method

--- app/src/main/java/com/unb/devapp/escambinho/LoginActivity.java
+++ app/src/main/java/com/unb/devapp/escambinho/LoginActivity.java
@@ -1,17 +1,34 @@
 package com.unb.devapp.escambinho;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
+import com.unb.devapp.escambinho.Helper.DatabaseFirebase.UserDatabaseHelper;
 
 public class LoginActivity extends AppCompatActivity {
 
-    boolean isAuth, isFirstAuth; // test
     MaterialButton mEntrar;
+
+    TextInputEditText mEmail, mSenha;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +36,13 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        isAuth = false;
-        isFirstAuth = true;
+        mAuth = FirebaseAuth.getInstance();
 
         mEntrar = findViewById(R.id.disabled_material_button);
+        mEmail = findViewById(R.id.login_edit_email);
+        mSenha = findViewById(R.id.login_edit_senha);
 
-        if(isAuth) {
+        if (mAuth.getCurrentUser() != null) {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
@@ -33,17 +51,71 @@ public class LoginActivity extends AppCompatActivity {
         mEntrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent;
-                if(isFirstAuth) {
-                    intent = new Intent(getApplicationContext(), SignUpActivity.class);
-                } else {
-                    intent = new Intent(getApplicationContext(), MainActivity.class);
-                }
-                startActivity(intent);
-                finish();
+
+                final String sEmail, sPass;
+
+                sEmail = mEmail.getText().toString();
+                sPass = mSenha.getText().toString();
+
+                signInVerify(sEmail, sPass);
+
             }
         });
 
 
+    }
+
+    private void signInVerify(final String sEmail, final String sPass) {
+        mAuth.signInWithEmailAndPassword(sEmail, sPass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("EmailSenha", "signInWithEmail:success");
+
+                            Toast.makeText(LoginActivity.this, "Bem-vindo de volta!",
+                                    Toast.LENGTH_SHORT).show();
+
+
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            UserDatabaseHelper.getUserWithEmail(sEmail, new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.getValue() == null) {
+                                        Toast.makeText(LoginActivity.this, "Bem-vindo ao Escambinho Unb!",
+                                                Toast.LENGTH_SHORT).show();
+
+                                        Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("email", sEmail);
+                                        bundle.putString("senha", sPass);
+                                        intent.putExtras(bundle);
+
+                                        startActivity(intent);
+
+                                        finish();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                }
+                            });
+
+                            Log.w("EmailSenha", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Senha incorreta!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
--- app/src/main/java/com/unb/devapp/escambinho/SignUpActivity.java
+++ app/src/main/java/com/unb/devapp/escambinho/SignUpActivity.java
@@ -1,21 +1,35 @@
 package com.unb.devapp.escambinho;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.unb.devapp.escambinho.Helper.DatabaseFirebase.UserDatabaseHelper;
 import com.unb.devapp.escambinho.Model.UserModel;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    UserModel userModel;
+    private FirebaseAuth mAuth;
 
-    MaterialButton mContinuar;
+    private UserModel userModel;
 
-    TextInputEditText mName, mBirth, mCourse, mMatr;
+    private MaterialButton mContinuar;
+
+    private TextInputEditText mName, mBirth, mCourse, mMatr;
+
+    private String bEmail, bPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +43,9 @@ public class SignUpActivity extends AppCompatActivity {
         mCourse = findViewById(R.id.signup_edit_curso);
         mMatr = findViewById(R.id.signup_edit_matr);
 
+        bEmail = getIntent().getExtras().getString("email");
+        bPass = getIntent().getExtras().getString("senha");
+
         mContinuar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -38,7 +55,43 @@ public class SignUpActivity extends AppCompatActivity {
                 userModel.setBirthday(mBirth.getText().toString());
                 userModel.setCourse(mCourse.getText().toString());
                 userModel.setId(mMatr.getText().toString());
+                userModel.setEmail(bEmail);
+
+                signUpVerify(userModel, bEmail, bPass);
             }
         });
+    }
+
+    private void signUpVerify(final UserModel userModel, String bEmail, String bPass) {
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.createUserWithEmailAndPassword(bEmail, bPass)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            UserDatabaseHelper.createUser(userModel, new OnSuccessListener() {
+                                @Override
+                                public void onSuccess(Object o) {
+                                    // Sign in success, update UI with the signed-in user's information
+                                    Log.d("EmailSenha", "createUserWithEmail:success");
+
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            });
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("EmailSenha", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Erro ao criar conta. Tente novamente.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
