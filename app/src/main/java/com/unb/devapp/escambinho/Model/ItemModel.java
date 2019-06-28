4056b8edcd129f1190df4d8e1ea5ef0774ee252d
Luan Pignata <luanpignat@gmail.com>
2019-06-28 02:08:10 -0300

    Create comunication for User

--- app/src/main/java/com/unb/devapp/escambinho/Model/ItemModel.java
+++ app/src/main/java/com/unb/devapp/escambinho/Model/ItemModel.java
@@ -2,4 +2,37 @@ package com.unb.devapp.escambinho.Model;
 
 public class ItemModel {
     String userId, title, imageUrl;
+
+    public ItemModel() {
+    }
+
+    public ItemModel(String userId, String title, String imageUrl) {
+        this.userId = userId;
+        this.title = title;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
