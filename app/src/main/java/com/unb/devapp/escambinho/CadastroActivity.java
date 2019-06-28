330dc5f59c23299c693da6b8eafe3b1bd1e870b3
jvmunhoz <jvmunhoz@yahoo.com.br>
2019-06-28 02:29:12 -0300

    create CadastroActvity

--- app/src/main/java/com/unb/devapp/escambinho/CadastroActivity.java
+++ app/src/main/java/com/unb/devapp/escambinho/CadastroActivity.java
@@ -3,13 +3,39 @@ package com.unb.devapp.escambinho;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CadastroActivity extends AppCompatActivity {
+
+    ImageView mImgUpload;
+    TextView mTxtTitulo;
+    Button mBtnCadastrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+
+        mImgUpload = findViewById(R.id.cadastro_img_upload);
+        mTxtTitulo = findViewById(R.id.cadastro_titulo);
+        mBtnCadastrar = findViewById(R.id.cadastro_btn_anunciar);
+    }
+
+    public void filler() {
+
+        mBtnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 
     
