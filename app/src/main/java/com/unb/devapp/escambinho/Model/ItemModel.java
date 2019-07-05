4cc50be4fa2a75809054d0a97a671d841c8b725e
jvmunhoz <jvmunhoz@yahoo.com.br>
2019-07-05 12:51:24 -0300

    put all data from the item registration screen into the database

--- app/src/main/java/com/unb/devapp/escambinho/Model/ItemModel.java
+++ app/src/main/java/com/unb/devapp/escambinho/Model/ItemModel.java
@@ -1,7 +1,7 @@
 package com.unb.devapp.escambinho.Model;
 
 public class ItemModel {
-    String id, userId, title, imageUrl;
+    String id, userId, title, imageUrl, autor, editora, edicao, ano, paginas, condicao, descricao, tags;
 
     public ItemModel() {
         imageUrl = "";
@@ -14,10 +14,18 @@ public class ItemModel {
         this.imageUrl = imageUrl;
     }
 
-    public ItemModel(String userId, String title, String imageUrl) {
+    public ItemModel(String userId, String title, String imageUrl, String autor, String editora, String edicao, String ano, String paginas, String condicao, String descricao, String tags) {
         this.userId = userId;
         this.title = title;
         this.imageUrl = imageUrl;
+        this.autor = autor;
+        this.editora = editora;
+        this.edicao = edicao;
+        this.ano = ano;
+        this.paginas = paginas;
+        this.condicao = condicao;
+        this.descricao = descricao;
+        this.tags = tags;
     }
 
     public String getUserId() {
@@ -50,5 +58,37 @@ public class ItemModel {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public String getEdicao() {
+        return edicao;
+    }
+
+    public String getAno() {
+        return ano;
+    }
+
+    public String getPaginas() {
+        return paginas;
+    }
+
+    public String getCondicao() {
+        return condicao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public String getTags() {
+        return tags;
     }
 }
